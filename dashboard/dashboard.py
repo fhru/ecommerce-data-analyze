92270fa90f5be70894ad631abb7192aa478ca8f3
fhru <fhrurhmn@gmail.com>
2025-03-08 22:45:44 +0700

    init

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,8 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
-from datetime import datetime, timedelta
+from datetime import timedelta
 sns.set(style='dark')
 
 # Load data
